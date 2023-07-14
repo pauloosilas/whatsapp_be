b960cc5b209f6e4177a97c4c30ea7e08e0a0a510
pauloosilas <paulo3356@gmail.com>
2023-07-14 17:28:43 -0300

    update auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,12 +2,16 @@ import express from "express"
 import trimRequest from "trim-request"
 
 import { login, logout, refreshToken, register } from "../controllers/auth.controller.js"
+import authMidddleware from "../middlewares/authMidddleware.js"
 
 const router = express.Router()
 
 router.route('/register').post(trimRequest.all, register)
 router.route('/login').post(trimRequest.all, login)
 router.route('/logout').post(trimRequest.all, logout)
-router.route('/refreshToken').post(trimRequest.all, refreshToken)
+router.route('/refreshtoken').post(trimRequest.all, refreshToken)
+router.route('/testinggauthMiddleware',).get(trimRequest.all, authMidddleware, (req, res) => {
+    res.send(req.user)
+})
  
 export default router
